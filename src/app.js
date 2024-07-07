b415c7d5bbc07eb99e5abc63aad7bd7477bbed6a
Anurag Kumar <98249668+anuragk0@users.noreply.github.com>
2024-07-08 00:42:10 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const hbs = require('hbs')
 forecast = require("./utils/forecast")
 geocode = require("./utils/geocode")
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 //Define paths for express config
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
